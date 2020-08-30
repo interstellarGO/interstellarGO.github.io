3b108bc95144579f3db6b2ed6b42da35ba94fd0d
OpsAhmad <alendra056@gmail.com>
2020-08-30 15:23:20 +0700

    Update banyak

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,12 @@ window.onscroll = function () {
   }
   prevScrollpos = currentScrollPos;
 };
+
+function myPopup() {
+  var x = document.getElementById("popup");
+  if (x.style.display == "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
