6a9ca3e6eb956b7dcd89c038917c3777c017805c
OpsAhmad <alendra056@gmail.com>
2020-09-16 18:35:56 +0700

    update content.html
    adding poup image preview

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,24 +11,28 @@ window.onscroll = function () {
   prevScrollpos = currentScrollPos;
 };
 
-function myPopup(parameter = 0) {
+function myPopup(parameter = 0, image) {
   var x = document.getElementById("popup");
   if (parameter == 1) {
+    document.getElementById("popup-img").src = image;
     document.getElementById("judul-text").innerHTML = " Go-Jack";
     document.getElementById("deskripsi-text").innerHTML =
       "Sebuah aplikasi yang kami buat dengan kerjasama Go-Jack sehingga kami bisa merealisasikan impian serta idealisme mereka.";
   }
   if (parameter == 2) {
+    document.getElementById("popup-img").src = image;
     document.getElementById("judul-text").innerHTML = " Pertamini";
     document.getElementById("deskripsi-text").innerHTML =
       "Sebuah aplikasi yang kami buat dengan kerjasama Petramini sehingga kami bisa merealisasikan impian serta idealisme mereka.";
   }
   if (parameter == 3) {
+    document.getElementById("popup-img").src = image;
     document.getElementById("judul-text").innerHTML = " Tokomedia";
     document.getElementById("deskripsi-text").innerHTML =
       "Sebuah aplikasi yang kami buat dengan kerjasama Tokomedia sehingga kami bisa merealisasikan impian serta idealisme mereka.";
   }
   if (parameter == 4) {
+    document.getElementById("popup-img").src = image;
     document.getElementById("judul-text").innerHTML = " Indonet";
     document.getElementById("deskripsi-text").innerHTML =
       "Sebuah aplikasi yang kami buat dengan kerjasama Indonet sehingga kami bisa merealisasikan impian serta idealisme mereka.";
